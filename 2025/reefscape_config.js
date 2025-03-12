107e834e1cf91019f8025f06da8f75342552eed9
AaronH1272 <127373383+AaronH1272@users.noreply.github.com>
2025-03-12 12:13:48 -0400

    Update reefscape_config.js

--- 2025/reefscape_config.js
+++ 2025/reefscape_config.js
@@ -15,7 +15,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2025MIKE2",
+      "defaultValue": "2025MIMCC",
       "required": "true"
     },
     { "name": "Match Level",
